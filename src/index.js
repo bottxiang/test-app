3fb8d49af20714cdf81b1670c03747ab36921ff1
Bo Hsiang <bottxiang@gmail.com>
2021-11-08 21:26:21 +0800

    use React.createElement

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import CreateComment from './create_comment.js'
 import {Post, Comment} from './post.js'
-
+import { CommentBox } from './commentbox.js';
 // class LikeButton extends React.Component {
 //   constructor(props) {
 //     super(props)
@@ -14,21 +14,46 @@ import {Post, Comment} from './post.js'
 //     return (<button onClick={() => this.setState({liked: true})}>LIKE</button>);
 //   }
 // }
+const data = {
+  post: {
+    id: 123,
+    content:
+      "What we hope ever to do with ease, we must first learn to do with diligence. — Samuel Johnson",
+    user: "Mark Thomas"
+  },
+  comments: [
+    {
+      id: 0,
+      user: "David",
+      content: "such. win."
+    },
+    {
+      id: 1,
+      user: "Haley",
+      content: "Love it."
+    },
+    {
+      id: 2,
+      user: "Peter",
+      content: "Who was Samuel Johnson?"
+    },
+    {
+      id: 3,
+      user: "Mitchell",
+      content: "@Peter get off Letters and do your homework"
+    },
+    {
+      id: 4,
+      user: "Peter",
+      content: "@mitchell ok :P"
+    }
+  ]
+};
 
-const App = React.createElement(Post, {
-  id: 1,
-  user: 'mark',
-  content: ' said: Let\'s say this is a post!'
-},
-React.createElement(
-  Comment,
-  {
-    id: 2,
-    user: 'bob',
-    content: ' commented: Wow! How cool!'
-  }),
-  React.createElement(CreateComment)
-);
+const App = React.createElement(CommentBox, {
+  post: data.post,
+  comments: data.comments
+});
 
 const node = document.querySelector("#root")
 
