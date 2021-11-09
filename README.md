4e1e331e998d7231fa0e03c57a66e410a0cd91a7
wsl-dell <bottxiang@gmail.com>
2021-11-09 10:49:57 +0800

    rewrite with jsx

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ```
 <script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
-  <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
-  <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
-  <script src="https://unpkg.com/prop-types@15.6/prop-types.js"></script>
+<script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
+<script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
+<script src="https://unpkg.com/prop-types@15.6/prop-types.js"></script>
 ```
 ```
 npm init -y
--- src/jsx/commentbox.js
+++ src/jsx/commentbox.js
@@ -1,56 +1,3 @@
-// import CreateComment from './create_comment.js'
-// import {Post, Comment} from './post.js'
-// import { CommentBox } from './commentbox.js';
-
-// class LikeButton extends React.Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {liked: false}
-//   }
-
-//   render() {
-//     if (this.state.liked == true) {
-//       return "You liked this."
-//     }
-//     return (<button onClick={() => this.setState({liked: true})}>LIKE</button>);
-//   }
-// }
-const data = {
-  post: {
-    id: 123,
-    content:
-      "What we hope ever to do with ease, we must first learn to do with diligence. — Samuel Johnson",
-    user: "Mark Thomas"
-  },
-  comments: [
-    {
-      id: 0,
-      user: "David",
-      content: "such. win."
-    },
-    {
-      id: 1,
-      user: "Haley",
-      content: "Love it."
-    },
-    {
-      id: 2,
-      user: "Peter",
-      content: "Who was Samuel Johnson?"
-    },
-    {
-      id: 3,
-      user: "Mitchell",
-      content: "@Peter get off Letters and do your homework"
-    },
-    {
-      id: 4,
-      user: "Peter",
-      content: "@mitchell ok :P"
-    }
-  ]
-};
-
 class CreateComment extends React.Component {
   constructor(props) {
     super(props)
@@ -101,6 +48,9 @@ class CreateComment extends React.Component {
 }
 
 class Post extends React.Component {
+  constructor(props) {
+    super(props)
+  }
   render() {
 
     return (
@@ -121,7 +71,7 @@ class Comment extends React.Component {
       <div className='comment'>
         <h2 className='commentAuthor'>
           {this.props.user}
-          <span className='commentContent'>{this.props.content}</span>
+          <span className='commentContent'>{this.props.conetnt}</span>
         </h2>
       </div>
     );
@@ -149,13 +99,13 @@ class CommentBox extends React.Component {
   render() {
     return (
       <div className='commentBox'>
-        <Post id={this.props.post.id} content={this.props.post.content} user={this.props.post.user}/>
+        <Post id={this.props.post.id} conetnt={this.props.post.content} user={this.props.post.user}/>
         {
           this.state.comments.map(function(comment) {
             return (
               <Comment key={comment.id}
               id={comment.id}
-              content={comment.content}
+              conetnt={comment.conetnt}
               user={comment.user}/>
             )
           })
@@ -171,9 +121,4 @@ CommentBox.propTypes = {
   comments: PropTypes.arrayOf(PropTypes.object)
 };
 
-const App = (
-  <CommentBox post={data.post} comments={data.comments}/>
-)
-const node = document.querySelector("#root")
-
-ReactDOM.render(App, node)
+export { CommentBox };
